dec899701c795effb00f46f7473a7d2c23399d74
jeonghyeon Hwang <hijihilin@gmail.com>
2022-11-26 03:06:43 +0900

    [FEAT] Add Initial Project

---
+++ SimpleCameraProject/SimpleCameraProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCameraProject
+//
+//  Created by 황정현 on 2022/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
